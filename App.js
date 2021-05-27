68a1f0b2844458908956c8ac03e2094c2fb184ab
m <mingh4hire@gmail.com>
2021-05-26 19:38:43 -0500

    asf

---
+++ App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Buttons} from './Buttons';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start workisad fsdaf sdng on your app!</Text>
+      <Text>Open up App.js to start workisad fsdaf sdng on your app!</Text>
+      <Text>Open up App.js to start workisad fsdaf sdng on your app!</Text>
+      <Text>Open up App.js to start workisad fsdaf sdng on your app!</Text>
+      <Text>Open up App.js to start workisad fsdaf sdng on your app!</Text>
+      <Text>Open up App.js to start workisad fsdaf sdng on your app!</Text>
+      <button style={{marginRight: spacing + 'em'}}>asdfadsf</button>
+      <StatusBar style="auto" />
+      <StatusBar style="auto" />
+      <StatusBar style="auto" />
+      <Buttons/>
+    </View>
+  );
+}
+const spacing = 3;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const styles2 = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Buttons.js
@@ -0,0 +1,9 @@
+export function Buttons(){
+
+    return <div>
+        <Button>asfd </Button>
+    </div>
+
+}
+
+export default Buttons;
